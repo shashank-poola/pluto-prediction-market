f055684574423dbbac07c9397a8c742e867a05e8
shashank-poola <shashankpoola123@gmail.com>
2025-12-15 13:03:41 +0530

    generated privy-auth-db

---
+++ packages/pluto-db/prisma/migrations/20251215073036_privy/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "privyId" TEXT NOT NULL,
+    "email" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_privyId_key" ON "User"("privyId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
